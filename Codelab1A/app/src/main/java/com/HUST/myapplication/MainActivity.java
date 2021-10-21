7078bc096beecf85a095f6716b9e4390d35d09c2
unknown <henrynam28@gmail.com>
2021-10-21 21:01:09 +0700

    Add Shared Preference to save coutn value

--- Codelab1A/app/src/main/java/com/HUST/myapplication/MainActivity.java
+++ Codelab1A/app/src/main/java/com/HUST/myapplication/MainActivity.java
@@ -3,6 +3,8 @@ package com.HUST.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TextView;
@@ -13,6 +15,8 @@ public class MainActivity extends AppCompatActivity {
     private Button buttonToast, buttonCount;
     private TextView textViewCount;
     private int count = 0;
+
+    SharedPreferences sharedPreferences;
 
     @SuppressLint("SetTextI18n")
     @Override
@@ -29,5 +33,27 @@ public class MainActivity extends AppCompatActivity {
             count++;
             textViewCount.setText("" + count);
         });
+
+        retrieveCount();
+    }
+
+    @Override
+    protected void onPause() {
+        saveCount();
+        super.onPause();
+    }
+
+    public void saveCount() {
+        sharedPreferences = getSharedPreferences("saveCount", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putInt("count", count);
+        editor.commit();
+    }
+
+    @SuppressLint("SetTextI18n")
+    public void retrieveCount() {
+        sharedPreferences = getSharedPreferences("saveCount", Context.MODE_PRIVATE);
+        count = sharedPreferences.getInt("count", 0);
+        textViewCount.setText("" + count);
     }
 }
